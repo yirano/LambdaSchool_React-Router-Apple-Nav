67ef70d4d308ade8c86efa496c3150fdfc091d8a
yirano <yirano@protonmail.ch>
2020-03-08 12:56:20 -0400

    Add data and images for mac and iPad

---
+++ apple-nav/data/ipad.jsx
@@ -0,0 +1,52 @@
+const ipad = [
+  {
+    id: 0,
+    item: "iPad Pro",
+    image: '/ipad_pro'
+  },
+  {
+    id: 1,
+    item: "iPad Air",
+    image: '/ipad_air'
+  },
+  {
+    id: 2,
+    item: "iPad",
+    image: '/ipad'
+  },
+  {
+    id: 3,
+    item: "iPad Mini",
+    image: '/ipad_mini'
+  },
+  {
+    id: 4,
+    item: "Compare",
+    image: '/compare'
+  },
+  {
+    id: 5,
+    item: "Apple Pencil",
+    image: '/apple_pencil'
+  },
+  {
+    id: 6,
+    item: 'Smart Keyboard',
+    image: '/smart_keyboard'
+  },
+  {
+    id: 7,
+    item: 'AirPods',
+    image: '/airpods'
+  },
+  {
+    id: 8,
+    item: 'Accessories',
+    image: '/accessories'
+  },
+  {
+    id: 9,
+    item: 'iPadOS',
+    image: '/ipadOS'
+  }
+]
