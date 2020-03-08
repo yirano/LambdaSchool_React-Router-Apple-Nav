658a1bf7cf915efb07b5cabb00870c69ff4b053e
yirano <yirano@protonmail.ch>
2020-03-08 19:37:30 -0400

    Figure out how to import images into component

--- apple-nav/src/Components/SubNav/SubNav.jsx
+++ apple-nav/src/Components/SubNav/SubNav.jsx
@@ -1,26 +1,33 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-
-const SubNav = (props) => {
+const SubNav = props => {
   const [subNav, setSubNav] = useState();
   const id = props.match.params.id;
   console.log(props.data[0].products);
   return (
     <div>
       {props.data.map(x => {
-        { console.log(x.products) }
-        return (x.name == id ?
+        {
+          console.log(x.products);
+        }
+        return x.name == id ? (
           <div>
             <ul>
               {x.products.map(product => {
-                return <li>{product.item}</li>
+                return (
+                  <li>
+                    <img
+                      src={require(`../../images/${id}/${product.image}.svg`)}
+                    />
+                  </li>
+                );
               })}
             </ul>
-          </div> : null
-        )
+          </div>
+        ) : null;
       })}
     </div>
-  )
-}
+  );
+};
 
 export default SubNav;
--- apple-nav/src/data/data.jsx
+++ apple-nav/src/data/data.jsx
@@ -7,52 +7,52 @@ const data = [
       {
         id: 0,
         item: "iPad Pro",
-        image: 'ipad_pro'
+        image: "ipad_pro"
       },
       {
         id: 1,
         item: "iPad Air",
-        image: 'ipad_air'
+        image: "ipad_air"
       },
       {
         id: 2,
         item: "iPad",
-        image: 'ipad'
+        image: "ipad"
       },
       {
         id: 3,
         item: "iPad Mini",
-        image: 'ipad_mini'
+        image: "ipad_mini"
       },
       {
         id: 4,
         item: "Compare",
-        image: 'compare'
+        image: "compare"
       },
       {
         id: 5,
         item: "Apple Pencil",
-        image: 'apple_pencil'
+        image: "apple_pencil"
       },
       {
         id: 6,
-        item: 'Smart Keyboard',
-        image: 'smart_keyboard'
+        item: "Smart Keyboard",
+        image: "smart_keyboard"
       },
       {
         id: 7,
-        item: 'AirPods',
-        image: 'airpods'
+        item: "AirPods",
+        image: "airpods"
       },
       {
         id: 8,
-        item: 'Accessories',
-        image: 'accessories'
+        item: "Accessories",
+        image: "accessories"
       },
       {
         id: 9,
-        item: 'iPadOS',
-        image: 'ipadOS'
+        item: "iPadOS",
+        image: "ipadOS"
       }
     ]
   },
@@ -61,48 +61,48 @@ const data = [
     products: [
       {
         id: 0,
-        item: 'iPhone 11 Pro',
-        image: 'iphone_11_pro'
+        item: "iPhone 11 Pro",
+        image: "iphone_11_pro"
       },
       {
         id: 1,
-        item: 'iPhone 11',
-        image: 'iphone_11'
+        item: "iPhone 11",
+        image: "iphone_11"
       },
       {
         id: 2,
-        item: 'iPhone Xr',
-        image: 'ipone_xr'
+        item: "iPhone Xr",
+        image: "iphone_xr"
       },
       {
         id: 3,
-        item: 'iPhone 8',
-        image: 'iphone_8'
+        item: "iPhone 8",
+        image: "iphone_8"
       },
       {
         id: 4,
-        item: 'Compare',
-        image: 'compare'
+        item: "Compare",
+        image: "compare"
       },
       {
         id: 5,
-        item: 'Apple Card',
-        image: 'applecard'
+        item: "Apple Card",
+        image: "applecard"
       },
       {
         id: 6,
-        item: 'Airpods',
-        image: 'airpods'
+        item: "Airpods",
+        image: "airpods"
       },
       {
         id: 7,
-        item: 'Accessories',
-        image: 'accessories'
+        item: "Accessories",
+        image: "accessories"
       },
       {
         id: 8,
-        item: 'iOS 13',
-        image: 'ios_13'
+        item: "iOS 13",
+        image: "ios_13"
       }
     ]
   },
@@ -111,61 +111,61 @@ const data = [
     products: [
       {
         id: 0,
-        item: 'MacBook Air',
-        image: 'macbook_air'
+        item: "MacBook Air",
+        image: "macbook_air"
       },
       {
         id: 1,
         item: 'MacBook Pro 13"',
-        image: 'macbook_pro_13'
+        image: "macbook_pro_13"
       },
       {
         id: 2,
         item: 'MacBook Pro 16"',
-        image: 'macbook_pro_16'
+        image: "macbook_pro_16"
       },
       {
         id: 3,
-        item: 'iMac',
-        image: 'imac'
+        item: "iMac",
+        image: "imac"
       },
       {
         id: 4,
-        item: 'iMac Pro',
-        image: 'imac_pro'
+        item: "iMac Pro",
+        image: "imac_pro"
       },
       {
         id: 5,
-        item: 'Mac Pro',
-        image: 'mac_pro'
+        item: "Mac Pro",
+        image: "mac_pro"
       },
       {
         id: 6,
-        item: 'Mac mini',
-        image: 'mac_mini'
+        item: "Mac mini",
+        image: "mac_mini"
       },
       {
         id: 7,
-        item: 'Compare',
-        image: 'mac_compare'
+        item: "Compare",
+        image: "mac_compare"
       },
       {
         id: 8,
-        item: 'Pro Display XDR',
-        image: 'mac_pro'
+        item: "Pro Display XDR",
+        image: "mac_pro"
       },
       {
         id: 9,
-        item: 'Accessories',
-        image: 'mac_acc'
+        item: "Accessories",
+        image: "mac_acc"
       },
       {
         id: 10,
-        item: 'Catalina',
-        image: 'mac_catalina'
+        item: "Catalina",
+        image: "mac_catalina"
       }
     ]
   }
-]
+];
 
 export default data;
